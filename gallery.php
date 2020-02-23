5a5e744abbe28b209afa070a3107ba6b236870fb
Mayank_MP5 <mayank5pande@gmail.com>
2020-02-23 22:26:37 +0530

    Gallery Page adde

---
+++ gallery.php
@@ -0,0 +1,96 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<title>Best Dental Clinic in Kalyan | Sai Clinic</title>
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+	<meta charset="utf-8">
+	<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<![endif]-->
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/main.css" id="color-switcher-link">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/animations.css">
+	<link rel="stylesheet" href="css/gallery.css">
+	<!--<link rel="stylesheet" href="css/fonts.css">
+	<script src="js/vendor/modernizr-2.6.2.min.js"></script>-->
+
+	<!--[if lt IE 9]>
+		<script src="js/vendor/html5shiv.min.js"></script>
+		<script src="js/vendor/respond.min.js"></script>
+		<script src="js/vendor/jquery-1.12.4.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	
+
+<div class="preloader">
+	<div class="preloader_image"></div>
+</div>
+
+
+
+
+<!-- wrappers for visual page editor and boxed version of template -->
+<div id="canvas">
+<div id="box_wrapper">
+
+<!-- template sections -->
+<?php include("header.php")?>
+<div class="container">
+    <div class="row m-3 bg-color">
+        <h4>Gallery</h4>
+    </div>
+    <div class="row m-3 card-deck">
+        <div class="card col-sm-12 col-md-4 m-3 img-card">
+            <img src="./images/gallery/02.jpg" alt="" class="card-img-top">
+        </div>
+        <div class="card col-sm-12 col-md-4 m-3 img-card">
+            <img src="./images/gallery/04.jpg" alt="" class="card-img-top">
+        </div>
+        <div class="card col-sm-12 col-md-4 m-3 img-card">
+            <img src="./images/gallery/05.jpg" alt="" class="card-img-top">
+        </div>
+        <div class="card col-sm-12 col-md-4 m-3 img-card">
+            <img src="./images/gallery/07.jpg" alt="" class="card-img-top">
+        </div>
+        <div class="card col-sm-12 col-md-4 m-3 img-card">
+            <img src="./images/gallery/08.jpg" alt="" class="card-img-top">
+        </div>
+        <div class="card col-sm-12 col-md-4 m-3 img-card">
+            <img src="./images/gallery/13.jpg" alt="" class="card-img-top">
+        </div>
+        <div class="card col-sm-12 col-md-4 m-3 img-card">
+            <img src="./images/gallery/14.jpg" alt="" class="card-img-top">
+        </div>
+        <div class="card col-sm-12 col-md-4 m-3 img-card">
+            <img src="./images/gallery/15.jpg" alt="" class="card-img-top">
+        </div>
+        <div class="card col-sm-12 col-md-4 m-3 img-card">
+            <img src="./images/gallery/16.jpg" alt="" class="card-img-top">
+        </div>
+    </div>
+</div>
+
+<?php include("footer.php")?>
+
+
+</div><!-- eof #box_wrapper -->
+</div><!-- eof #canvas -->
+
+		<script src="js/compressed.js"></script>
+		<script src="js/main.js"></script>
+		
+
+	</body>
+</html>
